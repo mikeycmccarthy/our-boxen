a8348e4c5f323e98af2860100c5ae865cfc9a0b6
Michael McCarthy <michael.mccarthy@GL04632M.local>
2014-05-07 09:21:58 +0100

    Now pulling down dotfiles

--- modules/people/manifests/mikeycmccarthy.pp
+++ modules/people/manifests/mikeycmccarthy.pp
@@ -3,6 +3,7 @@ class people::mikeycmccarthy {
   include people::mikeycmccarthy::applications
   include people::mikeycmccarthy::gui
   include people::mikeycmccarthy::homebrew
+  include people::mikeycmccarthy::shell
   include people::mikeycmccarthy::java
 
 }
---
+++ modules/people/manifests/mikeycmccarthy/shell.pp
@@ -0,0 +1,23 @@
+class people::mikeycmccarthy::shell {
+
+   $home = "/Users/${::boxen_user}"
+   $dotfiles = "${boxen::config::srcdir}/dotfiles"
+
+   repository { $dotfiles:
+     source => "mikeycmccarthy/dotfiles"
+   }
+
+   file { "${home}/.zshrc":
+     ensure  => link,
+     mode  => '0644',
+     target  => "${dotfiles}/.zshrc",
+     require => Repository[$dotfiles]
+   }
+
+   include zsh
+
+   repository { "/Users/${boxen_user}/.oh-my-zsh":
+    source => 'robbyrussell/oh-my-zsh',
+   }
+
+}
